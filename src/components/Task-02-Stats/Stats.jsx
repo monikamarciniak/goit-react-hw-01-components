54e2cefef7268a942fb4fde94c38aae5123026ad
Monika Marciniak <monixxa90@gmail.com>
2022-11-14 16:43:18 +0100

    stylowanie

--- src/components/Task-02-Stats/Stats.jsx
+++ src/components/Task-02-Stats/Stats.jsx
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
 import StatsCSS from './stats.module.css';
 
+function getRandomHexColor() {
+  let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return bgColor;
+}
+
 export const Stats = ({ title, stats }) => {
     return (<section className={StatsCSS.statistics}>
-        {/* Warunek, żeby h2 wyświetlał się tylko wtedy, gdy nie tytuł nie będzie pusty. Jak tytuł będzie pusty to pominie h2 - nagłówek h2 nie wyrenderuje się!*/}
+        
         {title = null ? "" : <h2 className={StatsCSS.title}>{title}</h2>}
         <ul className={StatsCSS.statList}>
             {stats.map(({ id, label, percentage }) => (
                 <li
                     className={StatsCSS.item}
                     key={id}
-                    style={{ backgroundColor: myBackgroundColor }}
-                    /*style={{ backgroundColor: randomColorInHex() }}*/
+                    style={{
+              backgroundColor: getRandomHexColor(),
+            }}
+            
                 >
                     <span className={StatsCSS.label} style={{ color: myTextColor }}>{label}</span>
                     <span className={StatsCSS.percentage} style={{ color: myTextColor }}>{percentage}%</span>
@@ -22,15 +29,7 @@ export const Stats = ({ title, stats }) => {
     )
 }
 
-/* Funkcja na całkowicie losowy kolor w HEX: Zrezygnowałem z niej, bo czasami są niwidoczne napisy
 
-const randomColorInHex = () => {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-};
-
-*/
-
-const myBackgroundColor = '#007aff';
 const myTextColor = '#fff';
 
 Stats.propTypes = {
